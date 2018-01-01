b88a064acee66874c9d75432b9aee655d065a9c7
Cherny <ChernyYang@outlook.com>
2018-01-01 15:00:01 +0800

    first initial

---
+++ app/src/main/java/com/cherny/clipnote/MainListActivity.kt
@@ -0,0 +1,12 @@
+package com.cherny.clipnote
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_list)
+    }
+}
